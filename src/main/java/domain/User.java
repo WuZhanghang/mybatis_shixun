e49292105d1dcf22144875ddc24893e49c8c48df
WuZhanghang <49017997+WuZhanghang@users.noreply.github.com>
2019-09-07 10:45:34 +0800

    Update User.java
    远端：不需要学习了

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -10,13 +10,14 @@ import java.util.List;
 @Data
 public class User {
     private Integer id;
-    private String school;
+    
     private String email;
     private String username;
     private Date birthday;
     private String address;
     private String sex;
     private List<Account> accounts;
+    
 
 
    
